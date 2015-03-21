a05554ef814c14ec9e0fad47df71ccd1d2bca7c3
Naomi Perry <perry.naomi@gmail.com>
2015-03-21 09:49:24 -0500

    started app, created home screen, added activities for when each button is pressed

---
+++ BeeSpelled/app/src/main/java/com/example/beespelled/beespelled/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.beespelled.beespelled;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void playButton(View view){
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
+    }
+
+    protected void wordsButton(View view){
+        Intent intent = new Intent(this, ListsActivity.class);
+        startActivity(intent);
+    }
+
+    protected void optionsButton(View view){
+        Intent intent = new Intent(this, OptionsActivity.class);
+        startActivity(intent);
+    }
+}
