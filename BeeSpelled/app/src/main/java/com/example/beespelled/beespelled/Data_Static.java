99e335c35a40b43814b358c2bf4c16a7cfbad240
tmlewalen <tmlewallen@crimson.ua.edu>
2015-04-07 14:41:57 -0500

    Static Data Added

---
+++ BeeSpelled/app/src/main/java/com/example/beespelled/beespelled/Data_Static.java
@@ -0,0 +1,33 @@
+package com.example.beespelled.beespelled;
+
+import android.content.Context;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by Tmlewallen on 4/7/15.
+ */
+public class Data_Static {
+
+
+
+
+    public static List<String> getListNames(Context c){
+        return null;
+    }
+
+    public static List<String> getWordsByList(Context c, String listName){
+        return null;
+    }
+
+    public static Word getWordData(Context c, String word){
+        return null;
+    }
+
+    public static void changeListName(Context c, String originalName, String newName){
+        return;
+    }
+
+
+
+//    public static void
+}
